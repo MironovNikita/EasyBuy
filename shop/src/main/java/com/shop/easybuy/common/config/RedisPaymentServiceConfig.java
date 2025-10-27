df5a8ed19ac6ad52d0bf958abb2eb84fab37398c
MironovNikita <nikit1739@yandex.ru>
2025-10-27 23:57:17 +0300

    security-oauth2-feature: oauth2-аутентификация сервиса платежей реализована

--- shop/src/main/java/com/shop/easybuy/common/config/RedisPaymentServiceConfig.java
+++ shop/src/main/java/com/shop/easybuy/common/config/RedisPaymentServiceConfig.java
@@ -2,6 +2,7 @@ package com.shop.easybuy.common.config;
 
 import com.shop.easybuy.client.api.payment.PaymentApi;
 import com.shop.easybuy.client.invoker.payment.ApiClient;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,19 +10,36 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.security.oauth2.client.ReactiveOAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.web.reactive.function.client.ServerOAuth2AuthorizedClientExchangeFilterFunction;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.time.Duration;
 
 @Configuration
 @Profile("!test")
+@RequiredArgsConstructor
 public class RedisPaymentServiceConfig {
+
+    private final ReactiveOAuth2AuthorizedClientManager authorizedClientManager;
 
     @Value("${base.client.redis.url}")
     private String baseRedisUrl;
 
+    @Value("${oauth2.default.registration.id}")
+    private String defaultClientRegistrationId;
+
     @Bean
     public ApiClient paymentApiClient() {
-        return new ApiClient()
+        var oauth2 = new ServerOAuth2AuthorizedClientExchangeFilterFunction(authorizedClientManager);
+        oauth2.setDefaultClientRegistrationId(defaultClientRegistrationId);
+
+        var webClient = WebClient.builder()
+                .baseUrl(baseRedisUrl)
+                .filter(oauth2)
+                .build();
+
+        return new ApiClient(webClient)
                 .setBasePath(baseRedisUrl);
     }
 
